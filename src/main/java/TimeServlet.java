66d34dcd93f716fe7bbf59c40cea401f6447acac
sergeyonischuk <36tXg8Le%>
2023-03-04 18:46:24 +0200

    init commit

--- src/main/java/TimeServlet.java
+++ src/main/java/TimeServlet.java
@@ -1,13 +1,12 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet (urlPatterns = "/time")
+@WebServlet(urlPatterns = "/time")
 public class TimeServlet extends HttpServlet {
 
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
--- src/main/java/TimezoneValidateFilter.java
+++ src/main/java/TimezoneValidateFilter.java
@@ -1,15 +1,25 @@
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
-@WebFilter (value = "/timeservlet/")
+@WebFilter("/timeservlet/")
 public class TimezoneValidateFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        super.doFilter(req, res, chain);
+        String timeZone = req.getParameter("timezone");
+        String regex = "^UTC[+-]{1}([0-9]|1[0-2])$";
+        if (timeZone.matches(regex)) {
+            chain.doFilter(req, res);
+        } else {
+            res.setStatus(401);
+            res.getWriter().write("invalid timezone");
+            res.getWriter().close();
+        }
     }
 }
