c57e50aed37690b86fb5a68e8db88e0be4b1353b
sergeyonischuk <sergey.onischuk@ukr.net>
2023-04-29 22:41:21 +0300

    init

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value = "/hello-world")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().write("Hello World");
+        resp.getWriter().close();
+    }
+}
